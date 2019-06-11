0638bc14d28f5dcbf16d876f4dfc666adb46c6cd
g4mbi <g4mbit7@gmail.com>
2019-06-11 11:25:43 +0300

    return context

--- src/main/java/com/heleta/football/FootballApplication.java
+++ src/main/java/com/heleta/football/FootballApplication.java
@@ -1,6 +1,5 @@
 package com.heleta.football;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
